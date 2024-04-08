39b5fa3ace89b99d7c9e0e47f98e1dfc6fd3fe3b
rvaldez20 <rvaldez20@gmail.com>
2024-04-07 20:33:49 -0600

    [ADD] start to change the custum hook for useReducer

---
+++ src/components/OrderTotals.tsx
@@ -0,0 +1,42 @@
+import { useMemo } from "react"
+import { OrderItem } from "../types"
+import { formatCurrency } from "../helpers"
+
+type OrderTotalsProps = {
+   order: OrderItem[],
+   tip: number,
+   placeOrder: () => void
+}
+
+
+export default function OrderTotals({order, tip, placeOrder}:OrderTotalsProps) {
+
+   const subTotalAmount = useMemo(() => order.reduce((total, item) => total + (item.price * item.quantity),0), [order])
+   const tipAmount = useMemo(() => subTotalAmount * tip ,[tip, order])
+   const totalAmount = useMemo(() => subTotalAmount + tipAmount, [tip, order])
+
+   return (
+      <>
+         <div className="space-y-3">
+            <h2 className="font-black text-2xl">Totales y Propina:</h2>
+            <p>Subtotal a pagar: {``}
+               <span className="font-bold">{formatCurrency(subTotalAmount)}</span>
+            </p>
+
+            <p>Propina: {``}
+               <span className="font-bold">{formatCurrency(tipAmount)}</span>
+            </p>
+
+            <p>Total a Pagar: {``}
+               <span className="font-bold">{formatCurrency(totalAmount)}</span>
+            </p>               
+         </div>
+
+         <button
+            className="w-full bg-black p-3 uppercase text-white font-bold rounded-lg disabled:opacity-25"
+            disabled={totalAmount === 0}  
+            onClick={placeOrder}          
+         >Guardar Orden</button>
+      </>
+   )
+}
---
+++ src/hooks/useOrder.ts
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import type { MenuItem, OrderItem } from '../types'
+
+export default function useOrder() {
+   const [order, setOrder] = useState<OrderItem[]>([])
+   const [tip, setTip] = useState(0)
+
+   const addItem = (item:MenuItem) => {
+      const itemExist = order.find(orderItem => orderItem.id === item.id)
+      if(itemExist) {
+         // ya existe y y ano lo agrega
+
+         // buscamos el item seleccionado e incrementamos su quantity
+         const updateOrder = order.map(orderItem => orderItem.id === item.id 
+            ? {...orderItem, quantity: orderItem.quantity + 1} 
+            : orderItem)
+            setOrder(updateOrder)
+      } else {
+         // no existe y lo agrega
+         const newItem = {...item, quantity: 1}
+         setOrder([...order, newItem])
+      }
+   }
+
+
+   const removeItem = (id: MenuItem['id']) => {
+      setOrder(order.filter(item => item.id !== id))
+   }
+
+
+   const placeOrder = () => {
+      setOrder([])
+      setTip(0)
+   }
+
+   return {
+      order,
+      tip, 
+      setTip,
+      addItem,
+      removeItem,
+      placeOrder,
+   }
+}
